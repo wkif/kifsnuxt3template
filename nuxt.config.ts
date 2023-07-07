97089d97dfb803a8a58196c3d20a1b6157632c86
wkif <kif101001000@163.com>
2023-07-07 10:13:05 +0800

    feat: add unocss with mdi and attributify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // eslint-disable-next-line no-undef
 export default defineNuxtConfig({
-  modules: ['@unocss/nuxt']
+  modules: ['@unocss/nuxt'],
+  unocss: { // icon: https://icones.js.org/   interactive: https://unocss.dev/interactive/
+    uno: true,
+    icons: true,
+    attributify: true,
+    shortcuts: {},
+    rules: []
+  }
 })
